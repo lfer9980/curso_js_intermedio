22a881ca3bbce78e899a6e145b4ed59923c20efa
lfer9980 <luis_fernandez99@hotmail.com>
2021-10-09 13:41:24 -0600

    clase 4: defineProperty

--- clase3/main.js
+++ clase3/main.js
@@ -14,13 +14,42 @@ console.log(Object.getOwnPropertyNames(juan)) */
 //nota al pie: tener cuidado con el ambito lexico de .entries
 /* console.log(Object.entries(juan)[3][0]) */
 
-console.log(Object.getOwnPropertyDescriptors(juan))
-
 
 //definir propiedades a objetos, con el super prototipo object
 Object.defineProperty(juan, "pruebaNASA", {
-	value: "Extraterrrestres",
+	value: "Extraterrestres",
+	writable: false,
+	enumerable: false,
+	configurable: false,
+})
+
+Object.defineProperty(juan, "navigator", {
+	value: "Chrome",
+	enumerable: false,
 	writable: true,
-	enumerable: true,
 	configurable: true,
 })
+
+Object.defineProperty(juan, "editor", {
+	value: "VSCode",
+	enumerable: true,
+	writable: false,
+	configurable: true,
+})
+
+Object.defineProperty(juan, "terminal", {
+	value: "WSL",
+	enumerable: true,
+	writable: true,
+	configurable: false,
+})
+
+//OTROS METODOS DE OBJECT
+Object.seal(juan)
+/* con seal, protegemos que se borren las propiedades, cambiando configurable a false */
+
+Object.freeze(juan)
+/* con freeze, protegemos que alguien modifique nuestro atributo, ademas de editarlas */
+
+
+console.log(Object.getOwnPropertyDescriptors(juan))
